8ef05dadb69bb6f89b210851574cdc054531302f
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-09 22:13:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = []
+
+for i in range(5):
+    numbers.append(int(input()))
+
+userInput = int(input())
+prev_num = numbers[0]
+
+# numbers.insert(2, 20)
+for y in range(5):
+    if userInput > prev_num and userInput < numbers[y]:
+        numbers.insert(y, userInput)
+        
+
+    prev_num = numbers[y]
+
+print(numbers)
